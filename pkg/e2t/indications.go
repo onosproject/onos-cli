725f9d7555448eaa7392961d9e1fbaa638ce253f
Thomas Vachuska <tom@opennetworking.org>
2021-07-07 21:01:26 -0700

    Partial watch indications code; dead-end on SM-awareness

--- pkg/e2t/indications.go
+++ pkg/e2t/indications.go
@@ -18,19 +18,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/onosproject/onos-api/go/onos/e2sub/subscription"
-	"github.com/onosproject/onos-api/go/onos/e2t/admin"
-	"github.com/onosproject/onos-ric-sdk-go/pkg/e2/creds"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"io"
-	"strconv"
-	"strings"
-	"time"
-
+	e2api "github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1"
 	"github.com/onosproject/onos-lib-go/pkg/cli"
-	e2client "github.com/onosproject/onos-ric-sdk-go/pkg/e2"
-	"github.com/onosproject/onos-ric-sdk-go/pkg/e2/indication"
+	e2client "github.com/onosproject/onos-ric-sdk-go/pkg/e2/v1beta1"
+	"strings"
 
 	"text/tabwriter"
 
@@ -44,82 +35,9 @@ func getWatchIndicationsCommand() *cobra.Command {
 		RunE:  runWatchIndicationsCommand,
 	}
 	cmd.Flags().Bool("no-headers", false, "disables output headers")
-	cmd.PersistentFlags().String("service-address", "onos-e2sub:5150", "the gRPC endpoint")
-	cmd.Flags().Duration("timeout", time.Hour, "specifies maximum wait time for new indications")
+	cmd.Flags().String("service-model", "oran-e2sm-rc-pre:v2", "service model as name:version")
+	cmd.Flags().String("node", "", "E2 node ID")
 	return cmd
-}
-
-// createSubscriptionRequest make a proto-encoded request for a subscription to indication data.
-// TODO : revisit this when JSON encoding is supported, and make this more general
-func createSubscriptionRequest(nodeID string) (subscription.SubscriptionDetails, error) {
-	return subscription.SubscriptionDetails{
-		E2NodeID: subscription.E2NodeID(nodeID),
-		ServiceModel: subscription.ServiceModel{
-			Name:    "oran-e2sm-kpm",
-			Version: "v1",
-		},
-		EventTrigger: subscription.EventTrigger{
-			Payload: subscription.Payload{
-				Encoding: subscription.Encoding_ENCODING_PROTO,
-				Data:     []byte{},
-			},
-		},
-		Actions: []subscription.Action{
-			{
-				ID:   100,
-				Type: subscription.ActionType_ACTION_TYPE_REPORT,
-				SubsequentAction: &subscription.SubsequentAction{
-					Type:       subscription.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
-					TimeToWait: subscription.TimeToWait_TIME_TO_WAIT_ZERO,
-				},
-			},
-		},
-	}, nil
-}
-
-const (
-	onosE2TAddress = "onos-e2t:5150"
-)
-
-// GetNodeIDs get list of E2 node IDs
-// TODO this function should be replaced with topology API
-func getNodeIDs() ([]string, error) {
-	tlsConfig, err := creds.GetClientCredentials()
-	var nodeIDs []string
-	if err != nil {
-		return []string{}, err
-	}
-	opts := []grpc.DialOption{
-		grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)),
-	}
-
-	conn, err := grpc.DialContext(context.Background(), onosE2TAddress, opts...)
-	if err != nil {
-		return []string{}, err
-	}
-	adminClient := admin.NewE2TAdminServiceClient(conn)
-	connections, err := adminClient.ListE2NodeConnections(context.Background(), &admin.ListE2NodeConnectionsRequest{})
-
-	if err != nil {
-		return []string{}, err
-	}
-
-	for {
-		connection, err := connections.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return []string{}, err
-		}
-		if connection != nil {
-			nodeID := connection.Id
-			nodeIDs = append(nodeIDs, nodeID)
-		}
-
-	}
-
-	return nodeIDs, nil
 }
 
 func runWatchIndicationsCommand(cmd *cobra.Command, args []string) error {
@@ -127,61 +45,54 @@ func runWatchIndicationsCommand(cmd *cobra.Command, args []string) error {
 	writer := new(tabwriter.Writer)
 	writer.Init(outputWriter, 0, 0, 3, ' ', tabwriter.FilterHTML)
 
-	timeout, _ := cmd.Flags().GetDuration("timeout")
-	address, _ := cmd.Flags().GetString("service-address")
-	tokens := strings.Split(address, ":")
-	if len(tokens) != 2 {
-		return errors.New("service-address must be of the form host:port")
+	sm, _ := cmd.Flags().GetString("service-model")
+	smf := strings.Split(sm, ":")
+	if len(smf) < 2 {
+		return errors.New("invalid model format")
 	}
-	host := tokens[0]
+	serviceModelName := e2client.ServiceModelName(smf[0])
+	serviceModelVersion := e2client.ServiceModelVersion(smf[1])
 
-	port, err := strconv.Atoi(tokens[1])
-	if err != nil {
-		return err
-	}
+	nodeID, _ := cmd.Flags().GetString("node")
 
-	clientConfig := e2client.Config{
-		AppID: "subscription-test",
-		SubscriptionService: e2client.ServiceConfig{
-			Host: host,
-			Port: port,
+	e2tHost := "onos-e2t"
+	e2tPort := 5150
+
+	client := e2client.NewClient(
+		e2client.WithServiceModel(serviceModelName, serviceModelVersion),
+		e2client.WithAppID("onos-cli"),
+		e2client.WithE2TAddress(e2tHost, e2tPort))
+
+	actions := []e2api.Action{
+		{
+			ID:   100,
+			Type: e2api.ActionType_ACTION_TYPE_REPORT,
+			SubsequentAction: &e2api.SubsequentAction{
+				Type:       e2api.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
+				TimeToWait: e2api.TimeToWait_TIME_TO_WAIT_ZERO,
+			},
 		},
 	}
 
-	client, err := e2client.NewClient(clientConfig)
+	trigger := e2api.EventTrigger{
+		Payload: []byte{},
+	}
+
+	ch := make(chan e2api.Indication)
+	node := client.Node(e2client.NodeID(nodeID))
+	subName := "onos-pci-subscription"
+	subSpec := e2api.SubscriptionSpec{
+		Actions:      actions,
+		EventTrigger: trigger,
+	}
+	ctx := context.Background()
+	channelID, err := node.Subscribe(ctx, subName, subSpec, ch)
 	if err != nil {
 		return err
 	}
 
-	ch := make(chan indication.Indication)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	nodeIDs, err := getNodeIDs()
-	if err != nil {
-		return err
-	}
-
-	subReq, err := createSubscriptionRequest(nodeIDs[0])
-	if err != nil {
-		return err
-	}
-
-	_, err = client.Subscribe(ctx, subReq, ch)
-	if err != nil {
-		return err
-	}
-
-	done := false
-	for !done {
-		select {
-		case indicationMsg := <-ch:
-			_, _ = fmt.Fprintf(writer, "%v\n\n", indicationMsg)
-			_ = writer.Flush()
-		case <-time.After(timeout):
-			done = true
-		}
-	}
+	// TODO: to be implemented
+	fmt.Printf("%v", channelID)
 
 	return nil
 }
