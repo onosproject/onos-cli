c0d0bed58b2e9b343892a5c7fbee3fd7dabf7c71
Francis Chua <francis@opennetworking.org>
2021-07-30 11:13:48 -0700

    refactor, bump

--- pkg/topo/get.go
+++ pkg/topo/get.go
@@ -165,96 +165,11 @@ func runGetObjectsCommand(cmd *cobra.Command, args []string) error {
 	to, _ := cmd.Flags().GetString("related-to")
 	via, _ := cmd.Flags().GetString("related-via")
 	tgt, _ := cmd.Flags().GetString("tgt-kind")
-	noHeaders, _ := cmd.Flags().GetBool("no-headers")
-	verbose, _ := cmd.Flags().GetBool("verbose")
-	sortString, _ := cmd.Flags().GetString("sort-order")
+
 	if len(to) != 0 || len(via) != 0 || len(tgt) != 0 {
-		verbose, _ := cmd.Flags().GetBool("verbose")
-		noHeaders, _ := cmd.Flags().GetBool("no-headers")
-		scopeString, _ := cmd.Flags().GetString("scope")
-		scope := topoapi.RelationFilterScope_TARGET_ONLY
-
-		if scopeString == "all" {
-			scope = topoapi.RelationFilterScope_ALL
-		} else if scopeString == "source_and_target" {
-			scope = topoapi.RelationFilterScope_SOURCE_AND_TARGET
-		}
-
-		if len(to) > 0 && len(via) > 0 {
-			outputWriter := cli.GetOutput()
-			writer := new(tabwriter.Writer)
-			writer.Init(outputWriter, 0, 0, 3, ' ', tabwriter.FilterHTML)
-			if !noHeaders {
-				printHeader(writer, topoapi.Object_RELATION, verbose, false)
-			}
-			if len(tgt) == 0 {
-				tgt = ""
-			}
-
-			objects, err := listObjects(cmd, &topoapi.Filters{
-				RelationFilter: &topoapi.RelationFilter{
-					SrcId:        to,
-					RelationKind: via,
-					TargetKind:   tgt,
-					Scope:        scope,
-				},
-			}, topoapi.SortOrder_UNORDERED)
-			if err == nil {
-				for _, object := range objects {
-					printObject(writer, object, verbose, false)
-				}
-			}
-			_ = writer.Flush()
-			return nil
-		}
-		return errors.NewInvalid("missing related-to and/or related-via flags")
+		return listObjectsRelations(cmd, to, via, tgt)
 	}
-
-	sortOrder := topoapi.SortOrder_UNORDERED
-	if sortString == "ascending" {
-		sortOrder = topoapi.SortOrder_ASCENDING
-	} else if sortString == "descending" {
-		sortOrder = topoapi.SortOrder_DESCENDING
-	} else if sortString == "unordered" {
-		sortOrder = topoapi.SortOrder_UNORDERED
-	}
-
-	outputWriter := cli.GetOutput()
-	writer := new(tabwriter.Writer)
-	writer.Init(outputWriter, 0, 0, 3, ' ', 0)
-	objects := make([]topoapi.Object, 0)
-	if len(args) == 0 {
-		filters := compileFilters(cmd, topoapi.Object_ENTITY)
-		filters.ObjectTypes = []topoapi.Object_Type{topoapi.Object_ENTITY, topoapi.Object_RELATION, topoapi.Object_KIND}
-
-		listedObjects, err := listObjects(cmd, filters, sortOrder)
-		if err != nil {
-			return err
-		}
-		objects = append(objects, listedObjects...)
-	} else {
-		id := args[0]
-		object, err := getObject(cmd, topoapi.ID(id))
-		if err != nil {
-			return err
-		}
-		objects = append(objects, *object)
-	}
-
-	if !noHeaders {
-		_, _ = fmt.Fprintf(writer, "%s\t%s\t%s\t%s\t%s\t%s", "Object Type", "Object ID", "Kind ID", "Source ID", "Target ID", "Labels")
-
-		if !verbose {
-			_, _ = fmt.Fprintf(writer, "\tAspects\n")
-		} else {
-			_, _ = fmt.Fprintf(writer, "\n")
-		}
-	}
-	for _, object := range objects {
-		printObject(writer, object, verbose, true)
-	}
-	_ = writer.Flush()
-	return nil
+	return listAllObjectTypes(cmd, args)
 }
 
 func runGetCommand(cmd *cobra.Command, args []string, objectType topoapi.Object_Type) error {
@@ -301,6 +216,99 @@ func runGetCommand(cmd *cobra.Command, args []string, objectType topoapi.Object_
 		}
 	}
 
+	_ = writer.Flush()
+	return nil
+}
+
+func listObjectsRelations(cmd *cobra.Command, to string, via string, tgt string) error {
+	verbose, _ := cmd.Flags().GetBool("verbose")
+	noHeaders, _ := cmd.Flags().GetBool("no-headers")
+	scopeString, _ := cmd.Flags().GetString("scope")
+	scope := topoapi.RelationFilterScope_TARGET_ONLY
+
+	if scopeString == "all" {
+		scope = topoapi.RelationFilterScope_ALL
+	} else if scopeString == "source_and_target" {
+		scope = topoapi.RelationFilterScope_SOURCE_AND_TARGET
+	}
+
+	if len(to) > 0 && len(via) > 0 {
+		outputWriter := cli.GetOutput()
+		writer := new(tabwriter.Writer)
+		writer.Init(outputWriter, 0, 0, 3, ' ', tabwriter.FilterHTML)
+		if !noHeaders {
+			printHeader(writer, topoapi.Object_RELATION, verbose, false)
+		}
+		if len(tgt) == 0 {
+			tgt = ""
+		}
+
+		objects, err := listObjects(cmd, &topoapi.Filters{
+			RelationFilter: &topoapi.RelationFilter{
+				SrcId:        to,
+				RelationKind: via,
+				TargetKind:   tgt,
+				Scope:        scope,
+			},
+		}, topoapi.SortOrder_UNORDERED)
+		if err == nil {
+			for _, object := range objects {
+				printObject(writer, object, verbose, false)
+			}
+		}
+		_ = writer.Flush()
+		return nil
+	}
+	return errors.NewInvalid("missing related-to and/or related-via flags")
+}
+
+func listAllObjectTypes(cmd *cobra.Command, args []string) error {
+	noHeaders, _ := cmd.Flags().GetBool("no-headers")
+	verbose, _ := cmd.Flags().GetBool("verbose")
+	sortString, _ := cmd.Flags().GetString("sort-order")
+	sortOrder := topoapi.SortOrder_UNORDERED
+	if sortString == "ascending" {
+		sortOrder = topoapi.SortOrder_ASCENDING
+	} else if sortString == "descending" {
+		sortOrder = topoapi.SortOrder_DESCENDING
+	} else if sortString == "unordered" {
+		sortOrder = topoapi.SortOrder_UNORDERED
+	}
+
+	outputWriter := cli.GetOutput()
+	writer := new(tabwriter.Writer)
+	writer.Init(outputWriter, 0, 0, 3, ' ', 0)
+	objects := make([]topoapi.Object, 0)
+	if len(args) == 0 {
+		filters := compileFilters(cmd, topoapi.Object_ENTITY)
+		filters.ObjectTypes = []topoapi.Object_Type{topoapi.Object_ENTITY, topoapi.Object_RELATION, topoapi.Object_KIND}
+
+		listedObjects, err := listObjects(cmd, filters, sortOrder)
+		if err != nil {
+			return err
+		}
+		objects = append(objects, listedObjects...)
+	} else {
+		id := args[0]
+		object, err := getObject(cmd, topoapi.ID(id))
+		if err != nil {
+			return err
+		}
+		objects = append(objects, *object)
+	}
+
+	if !noHeaders {
+		_, _ = fmt.Fprintf(writer, "%s\t%s\t%s\t%s\t%s\t%s", "Object Type", "Object ID", "Kind ID", "Source ID", "Target ID", "Labels")
+
+		if !verbose {
+			_, _ = fmt.Fprintf(writer, "\tAspects\n")
+		} else {
+			_, _ = fmt.Fprintf(writer, "\n")
+		}
+	}
+	for _, object := range objects {
+		printObject(writer, object, verbose, true)
+	}
 	_ = writer.Flush()
 	return nil
 }
@@ -362,10 +370,10 @@ func printHeader(writer io.Writer, objectType topoapi.Object_Type, verbose bool,
 	}
 }
 
-func printObject(writer io.Writer, object topoapi.Object, verbose bool, print_type bool) {
+func printObject(writer io.Writer, object topoapi.Object, verbose bool, printType bool) {
 	labels := utils.None(labelsAsCSV(object))
 
-	if print_type {
+	if printType {
 		_, _ = fmt.Fprintf(writer, "%s\t", object.Type)
 	}
 
