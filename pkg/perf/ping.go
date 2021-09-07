f13598d7aaf6b17f0966feb0b1fa0aef8a3ef6fc
Thomas Vachuska <tom@opennetworking.org>
2021-09-07 10:45:48 -0700

    Implemented perf ping-stream client

--- pkg/perf/ping.go
+++ pkg/perf/ping.go
@@ -17,6 +17,7 @@ package perf
 import (
 	"context"
 	"github.com/aybabtme/uniplot/histogram"
+	"io"
 	"math/rand"
 	"time"
 
@@ -43,11 +44,11 @@ func getStreamCommand() *cobra.Command {
 		Aliases: []string{"relations"},
 		Args:    cobra.MaximumNArgs(1),
 		Short:   "Ping request/response over a stream",
-		RunE:    runPingCommand, // runPingStreamCommand,
+		RunE:    runPingStreamCommand,
 	}
 	cmd.Flags().IntP("count", "c", 1000, "number of ping invocations")
 	cmd.Flags().Uint32P("size", "s", 4096, "size of the payload")
-	cmd.Flags().IntP("responses", "r", 1, "number of responses for each request")
+	cmd.Flags().Uint32P("responses", "r", 1, "number of responses for each request")
 	return cmd
 }
 
@@ -78,6 +79,59 @@ func runPingCommand(cmd *cobra.Command, args []string) error {
 	}
 	analyzeLatencies(latencies)
 	return nil
+}
+
+func runPingStreamCommand(cmd *cobra.Command, args []string) error {
+	count, _ := cmd.Flags().GetInt("count")
+	size, _ := cmd.Flags().GetUint32("size")
+	responses, _ := cmd.Flags().GetUint32("responses")
+
+	conn, err := cli.GetConnection(cmd)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	client := perfapi.NewPerfServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	latencies := make([]float64, 0, count*int(responses))
+	stream, err := client.PingStream(ctx)
+	if err != nil {
+		return err
+	}
+
+	waitc := make(chan struct{})
+	go func() {
+		for {
+			response, err := stream.Recv()
+			if err == io.EOF {
+				close(waitc)
+				return
+			}
+			if err != nil {
+				cli.Output("get error: %+v", err)
+				return
+			}
+			latencies = append(latencies, float64(uint64(time.Now().UnixNano())-response.Timestamp)/1000.0)
+		}
+	}()
+
+	for i := 0; i < count; i++ {
+		payload := createPayload(size)
+		err = stream.Send(&perfapi.PingRequest{Payload: payload, Timestamp: uint64(time.Now().UnixNano()), RepeatCount: responses})
+		if err != nil {
+			cli.Output("get error: %+v", err)
+			return err
+		}
+	}
+	err = stream.CloseSend()
+	<-waitc
+
+	analyzeLatencies(latencies)
+	return err
 }
 
 func createPayload(size uint32) *perfapi.Data {
