f0677b037ba39346b1f78104705c792dc3a41769
Woojoong Kim <woojoong.m.kim@gmail.com>
2021-10-05 16:04:39 -0700

    Add RSM CLI

---
+++ pkg/rsm/root.go
@@ -0,0 +1,45 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rsm
+
+import (
+	"github.com/onosproject/onos-lib-go/pkg/cli"
+	"github.com/spf13/cobra"
+)
+
+const (
+	configName     = "rsm"
+	defaultAddress = "onos-rsm:5150"
+)
+
+// Init is a hook called after cobra initialization
+func Init() {
+	// noop for now
+}
+
+// GetCommand returns the root command for the RAN service
+func GetCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "rsm {set/create/update/delete} [args]",
+		Short: "ONOS RSM subsystem commands",
+	}
+
+	cli.AddConfigFlags(cmd, defaultAddress)
+	cmd.AddCommand(getSetCommand())
+	cmd.AddCommand(getCreateCommand())
+	cmd.AddCommand(getUpdateCommand())
+	cmd.AddCommand(getDeleteCommand())
+	return cmd
+}
