66b719b75669e02f0e4b08ccef35ad9443b8ab9c
Woojoong Kim <woojoong.m.kim@gmail.com>
2022-02-24 00:25:53 -0800

    Add A1T CLI (#273)
    * Add A1T CLI
    
    * Fix linters

---
+++ pkg/a1t/root.go
@@ -0,0 +1,63 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package a1t
+
+import (
+	"github.com/onosproject/onos-lib-go/pkg/cli"
+	"github.com/spf13/cobra"
+	"time"
+)
+
+const (
+	configName     = "a1t"
+	defaultAddress = "onos-a1t:5150"
+
+	// TimeoutTimer is a timer time
+	TimeoutTimer = time.Second * 5
+)
+
+func init() {
+	cli.InitConfig(configName)
+}
+
+// Init is a hook called after cobra initialization
+func Init() {
+	// noop for now
+}
+
+// GetCommand returns the root command for the RAN service
+func GetCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "a1t {get} [args]",
+		Short: "ONOS a1t subsystem commands",
+	}
+
+	cli.AddConfigFlags(cmd, defaultAddress)
+	cmd.AddCommand(getGetCommand())
+	return cmd
+}
+
+func getGetCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "get {subscriptions/subscription/policy} [args]",
+		Short:   "Get command",
+		Aliases: []string{"list"},
+	}
+
+	cmd.AddCommand(getGetSubscriptionCommand())
+	cmd.AddCommand(getPolicyCommand())
+
+	return cmd
+}
