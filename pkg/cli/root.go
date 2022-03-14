faec5ed25824a5914d29981a25bc39b00fe582d0
Thomas Vachuska <tom@opennetworking.org>
2022-03-14 16:06:03 -0700

    Changing license headers (#275)
    * Changing license headers
    
    * Added more license headers; modified .gitignore
    
    * Updated docs
    
    * Added headers to manual docs
    
    * Modified .gitignore
    
    * Removed build-tools
    
    * Added license header to push-images
    
    * Fixed Makefile
    
    * Cleaned up makefile target

--- pkg/cli/root.go
+++ pkg/cli/root.go
@@ -1,16 +1,6 @@
-// Copyright 2019-present Open Networking Foundation.
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
 
 package cli
 
@@ -48,10 +38,16 @@ func Execute() {
 	}
 }
 
+const apacheLicense = "<!--\nSP" + "DX-FileCopyright" +
+	"Text: 2019-present Open Networking Foundation <info@opennetworking.org>\n\nSP" +
+	"DX-License-Identifier: Apache-2.0\n-->\n\n"
+
 // GenerateCliDocs generate markdown files for onos-cli commands
 func GenerateCliDocs() {
 	cmd := GetRootCommand()
-	err := doc.GenMarkdownTree(cmd, "docs/cli")
+	identity := func(s string) string { return s }
+	licenseStr := func(s string) string { return apacheLicense }
+	err := doc.GenMarkdownTreeCustom(cmd, "docs/cli", licenseStr, identity)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
