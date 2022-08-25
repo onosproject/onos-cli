af7ec11346b2c04a847cac2a350aeaf76d77c47b
adibrastegarnia <adib.rastegarnia@intel.com>
2022-08-25 15:42:26 -0700

    Add device-provisioner CLI

---
+++ pkg/deviceprovisioner/crud.go
@@ -0,0 +1,21 @@
+package deviceprovisioner
+
+import "github.com/spf13/cobra"
+
+func getGetCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get {pipelines} [args]",
+		Short: "Get pipelines",
+	}
+	cmd.AddCommand(getListPipelinesCommand())
+	return cmd
+}
+
+func getWatchCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "watch {pipelines} [args]",
+		Short: "Watch for updates to a config resource type",
+	}
+	cmd.AddCommand(getWatchPipelinesCommand())
+	return cmd
+}
