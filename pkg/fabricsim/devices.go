525a9595e298cf61fe6aa57df18bde6215626559
Thomas Vachuska <tom.vachuska@intel.com>
2022-11-08 16:21:56 -0800

    Fixed a potential DBZ issue

--- pkg/fabricsim/devices.go
+++ pkg/fabricsim/devices.go
@@ -422,10 +422,10 @@ func printConnection(c *simapi.Connection) {
 }
 
 func printStats(stats *simapi.IOStats) {
-	secs := uint32((stats.LastUpdateTime - stats.FirstUpdateTime) / 1000000000)
+	msecs := uint32((stats.LastUpdateTime - stats.FirstUpdateTime) / 1000000)
 	bc := stats.InBytes + stats.OutBytes
 	mc := stats.InMessages + stats.OutMessages
-	cli.Output("\t%16d %12d %12d %12d\n", bc, mc, bc/secs, mc/secs)
+	cli.Output("\t%16d %12d %12d %12d\n", bc, mc, bc/msecs/1000, mc/msecs/1000)
 }
 
 func printPort(p *simapi.Port) {
